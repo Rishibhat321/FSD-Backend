215e41bc717017d31ebd9231dbce2022f8771c3e
Rishib#321 <rishitab2209@gmail.com>
2025-02-21 15:31:24 +0530

    add changes

--- Day-4/p1.js
+++ Day-4/p1.js
@@ -1,11 +1,26 @@
 const http= require('http');
+const user=[];
 
 const server=http.createServer((req,res) => {
     res.writeHead(200, {"Content-Type": "application/json"});
     if(req.url== '/getdata' && req.method==='GET') 
     {
-        const data = {name: 'John Doe', age:30};
-        res.end(JSON.stringify(data));
+     //   const data = {name: 'John Doe', age:30};
+     //   res.end(JSON.stringify(data));
+     res.end(JSON.stringify(users));
+    }
+    else if(req.url==='/setdata' && req.method==='POST')
+    {
+        let body='';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            const newData = JSON.parse(body);
+            console.log('New data recieved:  ${JSON.stringify(newData)}');
+            users.push(newData);
+            res.end('Data set successfully');
+        });
     }
 
 });
