7e192d3cf96d62662c349d61e864daf89945c263
Rishib#321 <rishitab2209@gmail.com>
2025-02-21 14:57:17 +0530

    add changes

---
+++ Day-3/p8.js
@@ -0,0 +1,44 @@
+const http= require('http');
+
+const server=http.createServer(async(req,res) => {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    const data=await fetch("https://fakestoreapi.com/");
+    const products= await data.json();
+    const myhtml=`<html>
+    <head>
+    <title>Products</title>
+    <style>
+    .container{
+    border: 1px solid black;
+    background-color: aqua;
+    }
+     </style>
+    </head>
+    <body>
+    <h1>Products</h1>
+    ${
+        products.map((product) => {
+            return `<div class="container">
+            <h2>Title: ${product.title}</h2>
+            <p>Description: ${product.description}</p>
+            <p>Price: ${product.price}</p>
+            <img src= "${product.image}" width="200" height="250" alt="${product.title}"></img>
+            </div>`
+        })
+    }
+    </body>
+    </html>
+
+    res.end(myhtml);
+        })
+    }`
+   
+})
+
+server.listen(9000, (err) => {
+    if(err) 
+        console.log(err);
+    else
+    console.log('Server running at http://localhost:9000/');
+
+})
